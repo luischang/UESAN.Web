1f40d01337aca430b3003444a49a754de5c7ccaf
Luis Chang <luis.chang@outlook.com>
2023-06-03 08:48:10 -0500

    Added Register componentd

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,6 +11,10 @@ const routes = [
   path: '/',
   component: () => import('components/auth/LoginForm.vue'),
  },
+ {
+  path: '/register',
+  component: () => import('components/auth/RegisterForm.vue'),
+ },
   // Always leave this as last one,
   // but you can also remove it
   {
